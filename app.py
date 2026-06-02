a1d2d2d72ba0f0f04a0452d5032e511668b93060
Swarup Dewanjee <dewanjee.swarup@gmail.com>
2026-06-02 20:44:43 +0600

    Deploy DiaResFormer

--- app.py
+++ app.py
@@ -776,6 +776,8 @@ def health():
     })
 
 
+import os
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port)
